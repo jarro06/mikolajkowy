f5c1f8d80a58de0d9d8dc6b041d62dc2f9ba755b
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-08 09:57:46 +0100

    Create maven project

---
+++ src/main/java/de/allianz/mikolajkowy/MikolajkowyApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.mikolajkowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikolajkowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikolajkowyApplication.class, args);
+	}
+
+}
